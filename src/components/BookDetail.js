316d4d285779ba356f88d374435912c8af5793e1
TheCodeMage <mr.phyoekyawkyaw@gmail.com>
2025-05-29 11:11:22 +0630

    Second Commit

---
+++ src/components/BookDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookDetail = () => {
+    return (
+        <div>
+            <h2>Book Detail</h2>
+            <p>Details of the selected book will be displayed here.</p>
+        </div>
+    );
+};
+
+export default BookDetail;
---
+++ src/components/BookList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookList = () => {
+    return (
+        <div>
+            <h2>Book List</h2>
+            <p>List of books will be displayed here.</p>
+        </div>
+    );
+};
+
+export default BookList;
